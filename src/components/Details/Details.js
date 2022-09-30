8b00529629d181b497fd1624884b511650152cd4
hachinrayhan <rayhan.hachin@gmail.com>
2022-09-30 06:42:25 +0600

    user name and location added

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,16 +1,19 @@
-import { faUserLarge } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot, faUserLarge } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const Details = () => {
     return (
-        <div>
-            <h1>
+        <div className='d-flex'>
+            <h1 className='me-3'>
                 <FontAwesomeIcon icon={faUserLarge} />
             </h1>
-            <div>
-                <h4>Hasin Rayhan</h4>
-                {/* <FontAwesomeIcon icon={ } /> */}
+            <div className='lh-1'>
+                <h5>Hasin Rayhan</h5>
+                <div>
+                    <FontAwesomeIcon className='me-2' icon={faLocationDot} />
+                    <span>Dhaka, Bangladesh</span>
+                </div>
             </div>
         </div>
     );
